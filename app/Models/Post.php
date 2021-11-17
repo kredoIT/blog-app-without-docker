bc4a56f935b429cd1bbffa7145d427296d6f5bdb
Burt Karl Cabigas <bortsigan@users.noreply.github.com>
2021-11-17 13:08:41 +0800

    changes

---
+++ app/Models/Post.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use App\Models\User;
+use App\Models\Comment;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'title',
+        'body',
+        'image',
+    ];
+
+    /**
+     * Get the user that owns the post.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the comments that is owned by the post.
+     */
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+}
